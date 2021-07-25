bc16504e6477ea297dab8310d1e29e703d2b5ce1
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-07-25 09:39:18 +0900

    update schema

--- backend/app/migrations/20210724232830_create_users.up.sql
+++ backend/app/migrations/20210724232830_create_users.up.sql
@@ -4,5 +4,7 @@ CREATE TABLE users (
   name varchar(255) DEFAULT NULL,
   created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY(id)
+  PRIMARY KEY(id),
+  UNIQUE unique_index_email_on_users (`email`),
+  UNIQUE unique_index_name_on_users (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
