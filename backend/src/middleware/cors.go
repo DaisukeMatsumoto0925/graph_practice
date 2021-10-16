aaa05b3fa797dcec21be6ce4d34f22fab6ae3f8e
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-10-16 22:24:06 +0900

    update to custom cors

--- backend/src/middleware/cors.go
+++ backend/src/middleware/cors.go
@@ -1,15 +1,24 @@
 package middleware
 
 import (
-	"os"
-
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func NewCors() echo.MiddlewareFunc {
-	return middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{os.Getenv("CORS_ALLOW_ORIGIN")},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-	})
+	// return middleware.CORSWithConfig(middleware.CORSConfig{
+	// 	AllowOrigins: []string{os.Getenv("CORS_ALLOW_ORIGIN")},
+	// 	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	// })
+
+	return func(h echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Writer.Header().Set("Access-Control-Allow-Origin", c.Request().Header.Get("Origin"))
+			c.Response().Header().Set("Access-Control-Max-Age", "12h0m0s")
+			c.Response().Header().Set("Access-Control-Allow-Methods", "POST, GET")
+			c.Response().Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Authorization")
+			c.Response().Header().Set("Access-Control-Expose-Headers", "Content-Length")
+			c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
+			return h(c)
+		}
+	}
 }
