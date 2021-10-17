8ca3b53a1866808187940cd1d803b5854552ba36
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-10-17 09:57:36 +0900

    updated

--- backend/src/middleware/cors.go
+++ backend/src/middleware/cors.go
@@ -1,11 +1,14 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo"
 )
 
 func NewCors() echo.MiddlewareFunc {
-	return func(h echo.HandlerFunc) echo.HandlerFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+
 		return func(c echo.Context) error {
 			c.Response().Writer.Header().Set("Access-Control-Allow-Origin", c.Request().Header.Get("Origin"))
 			c.Response().Header().Set("Access-Control-Max-Age", "12h0m0s")
@@ -13,7 +16,16 @@ func NewCors() echo.MiddlewareFunc {
 			c.Response().Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Authorization")
 			c.Response().Header().Set("Access-Control-Expose-Headers", "Content-Length")
 			c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
-			return h(c)
+
+			if err := next(c); err != nil {
+				c.Error(err)
+			}
+
+			if c.Request().Method == http.MethodOptions {
+				return c.NoContent(http.StatusNoContent)
+			}
+
+			return next(c)
 		}
 	}
 }
